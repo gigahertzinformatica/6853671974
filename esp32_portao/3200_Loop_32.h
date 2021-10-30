413f433e2dd3ff6e89d5bd9445e384f2e0f3c848
gigahertzinformatica <72422672+gigahertzinformatica@users.noreply.github.com>
2021-10-30 14:50:25 -0300

    Add files via upload

---
+++ esp32_portao/3200_Loop_32.h
@@ -0,0 +1,8 @@
+{
+  if (button_boot.pressed) { //to connect wifi via Android esp touch app 
+    Serial.println("Firmware update Starting..");
+    firmwareUpdate();
+    button_boot.pressed = false;
+  }
+  repeatedCall();
+}
