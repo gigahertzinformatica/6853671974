00fa89fe054f973f73099420df08d6ed24d789fb
gigahertzinformatica <72422672+gigahertzinformatica@users.noreply.github.com>
2021-11-04 21:48:55 -0300

    Delete esp32_portao directory

--- esp32_portao/3200_Loop_32.h
+++
@@ -1,8 +0,0 @@
-{
-  if (button_boot.pressed) { //to connect wifi via Android esp touch app 
-    Serial.println("Firmware update Starting..");
-    firmwareUpdate();
-    button_boot.pressed = false;
-  }
-  repeatedCall();
-}
